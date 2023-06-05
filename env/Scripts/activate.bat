a754224df4b5420c3285591df597d5175730fd66
PC <you@example.com>
2023-06-05 10:34:29 +0200

    African Lango

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\AMAZON\ALU-BSE\African-Lingo\env
+set VIRTUAL_ENV=C:\Users\DELL\AndroidStudioProjects\Alx-portfolioProject\African-Lingo\env
 
 if not defined PROMPT set PROMPT=$P$G
 
